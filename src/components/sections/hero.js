27973cb3171ff04fdcf2dc0cace168923cc35f79
Abhinay Pandey <abhinay@saaspay.app>
2023-01-06 01:32:28 +0530

    about me

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,16 +60,16 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
+  const two = <h2 className="big-heading">Abhinay Pandey.</h2>;
   const three = <h3 className="big-heading">I build things for the web.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
+        I’m a web developer specializing in building exceptional
+        web experiences. Currently I am a student of Computer Science & Artificial Intelligence at
         at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
+        <a href="https://iiitl.ac.in/" target="_blank" rel="noreferrer">
+          IIIT Lucknow
         </a>
         .
       </p>
